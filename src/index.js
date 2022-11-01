16ba9d55e53d48bae02ce3379d176c73713c3280
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-11-01 16:11:45 +0100

    Add site download without cms

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./style.css"
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+
+        new CopyPlugin({
+            patterns: [
+              { from: "src/uploads", to: "images" },
+            ],
+          }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
